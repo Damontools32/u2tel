97fbec502d755a97a119041f6dba4969a432d97c
Damontools32 <123245734+Damontools32@users.noreply.github.com>
2023-07-13 13:49:22 +0330

    Update upload_to_telegram_bot.py

--- upload_to_telegram_bot.py
+++ upload_to_telegram_bot.py
@@ -1,4 +1,5 @@
 import os
+import zipfile
 import logging
 from telethon import TelegramClient, events
 
@@ -15,24 +16,27 @@ downloads_folder = '/root/downloads'
 
 client = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
 
-def get_all_files(directory):
-    for root, _, files in os.walk(directory):
-        for file in files:
-            if file != ".torrent.bolt.db":
-                yield os.path.join(root, file)
+def zip_directory(directory, zip_file):
+    with zipfile.ZipFile(zip_file, 'w', zipfile.ZIP_DEFLATED) as zipf:
+        for root, _, files in os.walk(directory):
+            for file in files:
+                if file != ".torrent.bolt.db":
+                    file_path = os.path.join(root, file)
+                    zipf.write(file_path, os.path.relpath(file_path, directory))
 
 @client.on(events.NewMessage(pattern='/send'))
 async def upload_files(event):
     logger.debug('Received /send command')
-    for file_path in get_all_files(downloads_folder):
-        file_name = os.path.basename(file_path)
 
-        try:
-            with open(file_path, 'rb') as f:
-                await client.send_file(event.chat_id, f, caption=file_name)
-        except IsADirectoryError as e:
-            await event.reply(f'Error uploading {file_name}: {e}')
-        except Exception as e:
-            await event.reply(f'Error uploading {file_name}: {e}')
+    zip_file = 'downloads.zip'
+    zip_directory(downloads_folder, zip_file)
+
+    try:
+        with open(zip_file, 'rb') as f:
+            await client.send_file(event.chat_id, f, caption=zip_file)
+    except Exception as e:
+        await event.reply(f'Error uploading {zip_file}: {e}')
+    finally:
+        os.remove(zip_file)
 
 client.run_until_disconnected()
